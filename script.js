6d87e271323d60bbd553145d6a62a0ce3c2fabac
Amit Thing <amittamang423@gmail.com>
2022-03-14 16:15:37 +0545

    login page

--- script.js
+++ script.js
@@ -19,91 +19,105 @@
 // window.addEventListener('resize', slideImage);
 
 
+// // to show other forms according to type
+// function showOtherForm() {
+//     const select = document.querySelector('.select-type')
+//     const typeDiv = document.querySelector('.type-div')
+//     const other = document.querySelector('.other')
+//     let html;
 
-function showOtherForm() {
-    const select = document.querySelector('.select-type')
-    const typeDiv = document.querySelector('.type-div')
-    const other = document.querySelector('.other')
-    let html;
-    if (select.value === 'house') {
-        html = `
-    <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <div class="form-group">
-        <label class="profile_details_text">Plinth Area:</label>
-        <input type="text" name="plinth_area" class="form-control" value="" required>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <div class="form-group">
-        <label class="profile_details_text">Bedrooms:</label>
-        <input type="text" name="bedroom" class="form-control" value="" required>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <div class="form-group">
-        <label class="profile_details_text">Bathroom:</label>
-        <input type="text" name="Bathroom" class="form-control" value="" required>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="row">
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-  <div class="form-group">
-    <label class="profile_details_text">Total rooms:</label>
-    <input type="text" name="total_rooms" class="form-control" value="" required>
-  </div>
-</div>
-</div>
-<div class="row">
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-  <div class="form-group">
-    <label class="profile_details_text">Floors:</label>
-    <input type="text" name="total_floors" class="form-control" value="" required>
-  </div>
-</div>
-</div>
-<div class="row">
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 submit">
-  <div class="form-group">
-    <input type="submit" class="btn btn-success" value="Submit">
-  </div>
-</div>
-</div>
-    `
-    } else {
-        html = `
-    <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <div class="form-group">
-        <label class="profile_details_text">Land area:</label>
-        <input type="text" name="land_area" class="form-control" value="" required>
-      </div>
-    </div>
-  </div>
+//     if (select.value === 'house') {
+//         html = `
+//     <div class="row">
+//     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//       <div class="form-group">
+//         <label class="profile_details_text">Plinth Area:</label>
+//         <input type="text" name="plinth_area" class="form-control" value="" required>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="row">
+//     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//       <div class="form-group">
+//         <label class="profile_details_text">Bedrooms:</label>
+//         <input type="text" name="bedroom" class="form-control" value="" required>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="row">
+//     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//       <div class="form-group">
+//         <label class="profile_details_text">Bathroom:</label>
+//         <input type="text" name="Bathroom" class="form-control" value="" required>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// <div class="row">
+// <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//   <div class="form-group">
+//     <label class="profile_details_text">Total rooms:</label>
+//     <input type="text" name="total_rooms" class="form-control" value="" required>
+//   </div>
+// </div>
+// </div>
+// <div class="row">
+// <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//   <div class="form-group">
+//     <label class="profile_details_text">Floors:</label>
+//     <input type="text" name="total_floors" class="form-control" value="" required>
+//   </div>
+// </div>
+// </div>
+// <div class="row">
+// <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 submit">
+//   <div class="form-group">
+//     <input type="submit" class="btn btn-success" value="Submit">
+//   </div>
+// </div>
+// </div>
+//     `
+//     } else {
+//         html = `
+//     <div class="row">
+//     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//       <div class="form-group">
+//         <label class="profile_details_text">Land area:</label>
+//         <input type="text" name="land_area" class="form-control" value="" required>
+//       </div>
+//     </div>
+//   </div>
 
-  <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <div class="form-group">
-        <label class="profile_details_text">Road width:</label>
-        <input type="text" name="road_width" class="form-control" value="" required>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 submit">
-  <div class="form-group">
-    <input type="submit" class="btn btn-success" value="Submit">
-  </div>
-</div>
-</div>
-    `
-    }
-    other.innerHTML = html
+//   <div class="row">
+//     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+//       <div class="form-group">
+//         <label class="profile_details_text">Road width:</label>
+//         <input type="text" name="road_width" class="form-control" value="" required>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="row">
+// <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 submit">
+//   <div class="form-group">
+//     <input type="submit" class="btn btn-success" value="Submit">
+//   </div>
+// </div>
+// </div>
+//     `
+//     }
+//     other.innerHTML = html
 
-}
+// }
+
+
+
+//for sign up form animation
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.querySelector('.signup-container');
+
+signUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active'));
